9b8c50a54ba956e3a07106f169b8612d132291db
egreenfield323 <evangreenfield@live.com>
2023-03-15 17:34:16 -0400

    Add test class to test upcoming_games_list_test.dart

---
+++ test/upcoming_games_list_test.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:game_data/upcoming_games_list_creator.dart';
+
+void main() {
+  final testFile = File('test/upcoming_games.json');
+  final stringData = testFile.readAsStringSync();
+  final decodedJsonObject = jsonDecode(stringData);
+
+  test('i can get the first name from the list of upcoming games', () {
+    UpcomingGameListCreator gameListCreator = UpcomingGameListCreator();
+    var gameList = gameListCreator.createListOfNames(decodedJsonObject);
+    var firstName = decodedJsonObject["results"][0]["name"];
+    expect(gameList[0], firstName);
+  });
+}
