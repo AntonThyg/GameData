c08f80328755cf25e10802bc621a9a0750738098
egreenfield323 <evangreenfield@live.com>
2023-03-15 19:25:22 -0400

    Added fixes for ':' in the url building

--- lib/network_access.dart
+++ lib/network_access.dart
@@ -3,6 +3,12 @@ class NetworkAccess {
     if (gameTitle.contains(' ')) {
       gameTitle = gameTitle.replaceAll(' ', '-');
     }
+    if (gameTitle.contains(':')) {
+      gameTitle = gameTitle.replaceAll(':', '');
+    }
+    if (gameTitle.contains(';')) {
+      gameTitle = gameTitle.replaceAll(';', '');
+    }
     return 'https://api.rawg.io/api/games/$gameTitle?key=8ecd5215484b49d1af3cb570778ce30f';
   }
 
