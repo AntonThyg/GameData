8370a09b3ddc19fc5f4ef32631eefbc9aa7c433b
spencersmith24 <spencersmith24@outlook.com>
2023-03-20 14:06:38 -0400

    Update API key

--- lib/network_access.dart
+++ lib/network_access.dart
@@ -9,13 +9,13 @@ class NetworkAccess {
     if (gameTitle.contains(';')) {
       gameTitle = gameTitle.replaceAll(';', '');
     }
-    return 'https://api.rawg.io/api/games/$gameTitle?key=89f628d60c034ca7990b4b7fdd270922';
+    return 'https://api.rawg.io/api/games/$gameTitle?key=33b90247a9b44a59b04d9d1e5871cac0';
   }
 
   String queryUpcoming() {
     var currentDate = DateTime.now().toString().split(' ')[0];
     var endDate =
         DateTime.now().add(const Duration(days: 365)).toString().split(' ')[0];
-    return 'https://api.rawg.io/api/games?dates=$currentDate,$endDate&ordering=released&key=89f628d60c034ca7990b4b7fdd270922';
+    return 'https://api.rawg.io/api/games?dates=$currentDate,$endDate&ordering=released&key=33b90247a9b44a59b04d9d1e5871cac0';
   }
 }
