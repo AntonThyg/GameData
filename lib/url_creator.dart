4be30342b3bfe00154d971316f1c67c2c90472c9
Spencer Smith <spencersmith24@outlook.com>
2023-03-24 11:00:17 -0400

    hide api key

--- lib/url_creator.dart
+++ lib/url_creator.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 class UrlCreator {
-  final String _key = File("lib/key.gitignore").readAsStringSync();
+  // final String _key = File("lib/key.gitignore").readAsStringSync();
 
   String createSpecificQueryUrl(String gameTitle) {
     if (gameTitle.contains(' ')) {
@@ -13,13 +13,13 @@ class UrlCreator {
     if (gameTitle.contains(';')) {
       gameTitle = gameTitle.replaceAll(';', '');
     }
-    return 'https://api.rawg.io/api/games/$gameTitle?key=$_key';
+    return 'https://api.rawg.io/api/games/$gameTitle?key=';
   }
 
   String createUpcomingGamesQueryUrl() {
     String currentDate = DateTime.now().toString().split(' ')[0];
     String endDate =
         DateTime.now().add(const Duration(days: 365)).toString().split(' ')[0];
-    return 'https://api.rawg.io/api/games?dates=$currentDate,$endDate&ordering=released&key=$_key';
+    return 'https://api.rawg.io/api/games?dates=$currentDate,$endDate&ordering=released&key=';
   }
 }
