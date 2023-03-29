975a252b59e8b377ea590026701b74b2fb2c02af
Evan Greenfield <evangreenfield@live.com>
2023-03-29 16:39:53 -0400

    filter out itch.io games for user searching

--- lib/url_creator.dart
+++ lib/url_creator.dart
@@ -13,13 +13,13 @@ class UrlCreator {
     if (gameTitle.contains(';')) {
       gameTitle = gameTitle.replaceAll(';', '');
     }
-    return 'https://api.rawg.io/api/games/$gameTitle?key=$_key';
+    return 'https://api.rawg.io/api/games/$gameTitle?stores=1,2,3,4,5,6,7,8,11&key=$_key';
   }
 
   String createUpcomingGamesQueryUrl() {
     String currentDate = DateTime.now().toString().split(' ')[0];
     String endDate =
         DateTime.now().add(const Duration(days: 365)).toString().split(' ')[0];
-    return 'https://api.rawg.io/api/games?dates=$currentDate,$endDate&ordering=released?stores=1,2,3,4,5,6,7,8,11&key=$_key';
+    return 'https://api.rawg.io/api/games?dates=$currentDate,$endDate&stores=1,2,3,4,5,6,7,8,11&ordering=released&key=$_key';
   }
 }
