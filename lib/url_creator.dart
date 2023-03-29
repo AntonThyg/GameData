ce9e91c5fb3d3d859b15ac246de66b5d9d56d5e9
Evan Greenfield <evangreenfield@live.com>
2023-03-29 16:16:19 -0400

    filter out itch.io games in our API request link

--- lib/url_creator.dart
+++ lib/url_creator.dart
@@ -20,6 +20,6 @@ class UrlCreator {
     String currentDate = DateTime.now().toString().split(' ')[0];
     String endDate =
         DateTime.now().add(const Duration(days: 365)).toString().split(' ')[0];
-    return 'https://api.rawg.io/api/games?dates=$currentDate,$endDate&ordering=released&key=$_key';
+    return 'https://api.rawg.io/api/games?dates=$currentDate,$endDate&ordering=released?stores=1,2,3,4,5,6,7,8,11&key=$_key';
   }
 }
