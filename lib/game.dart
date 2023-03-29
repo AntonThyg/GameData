a3bf01f819360db6be7929b4e9c860e8e141d890
Spencer Smith <spencersmith24@outlook.com>
2023-03-29 15:44:01 -0400

    add instance variable releaseDateISO and releaseDateString to Game object

--- lib/game.dart
+++ lib/game.dart
@@ -2,9 +2,10 @@ class Game {
   final String title;
   final String description;
   final int rating;
-  final DateTime releaseDate;
+  final DateTime releaseDateISO;
+  final String releaseDateString;
   final String imageUrl;
 
-  Game(this.title, this.description, this.rating, this.releaseDate,
-      this.imageUrl);
+  Game(this.title, this.description, this.rating, this.releaseDateISO,
+      this.releaseDateString, this.imageUrl);
 }
