e70f15650631ad31d6fe16dd369a234791b01247
Antone Thygerson <arthygerson@bsu.edu>
2023-03-29 13:56:36 -0400

    Updated User Interface

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,72 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: '10 Soonest Upcoming Games',
       theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-        canvasColor: Colors.white,
+        primarySwatch: Colors.grey,
+        canvasColor: Colors.white60,
       ),
-      home: const Scaffold(body: GameDataWidget()),
+      home: const Scaffold(body: MyHomePage()),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = const GameDataWidget();
+        break;
+      case 1:
+        page = const SearchPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth >= 600,
+                  destinations: const [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.watch_later_outlined),
+                      label: Text('Upcoming'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.search),
+                      label: Text('Search'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.white60,
+                  child: page,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
@@ -36,35 +98,59 @@ class _GameDataWidgetState extends State<GameDataWidget> {
   List<Game> upcomingGamesList = [];
 
   @override
+  void initState() {
+    if (upcomingGamesList.isEmpty) {
+      makeUpcomingGameList();
+    }
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final widgetList = makeWidgetList();
-    return ListView(padding: const EdgeInsets.all(8), children: [
-      for (Widget w in widgetList) w,
-    ]);
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: [
+        for (Widget w in widgetList) w,
+      ],
+    );
   }
 
   List<Widget> makeWidgetList() {
-    makeUpcomingGameList();
+    //makeUpcomingGameList();
 
     if (upcomingGamesList.isNotEmpty) {
       return List.generate(
-          10,
-          (index) => Column(
-                children: [
-                  Image.network(upcomingGamesList[index].imageUrl),
-                  Text(upcomingGamesList[index].title),
-                  Text(upcomingGamesList[index].description),
-                  Text(upcomingGamesList[index].releaseDate.toString()),
-                ],
-              ));
+        10,
+        (index) => Column(
+          children: [
+            Row(
+              children: [
+                SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: Image.network(upcomingGamesList[index].imageUrl),
+                ),
+                Column(
+                  children: [
+                    Text(upcomingGamesList[index].title),
+                    Text(upcomingGamesList[index].releaseDate.toString()),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      );
     }
     return List.generate(
-        1,
-        (index) => Column(
-              children: const [
-                Text('Loading data...'),
-              ],
-            ));
+      1,
+      (index) => Column(
+        children: const [
+          Text('Loading data...'),
+        ],
+      ),
+    );
   }
 
   Future<void> makeUpcomingGameList() async {
@@ -77,8 +163,66 @@ class _GameDataWidgetState extends State<GameDataWidget> {
 
     List<Game> upcomingGames =
         await listCreator.createListOfGames(upcomingSlugs);
-    setState(() {
-      upcomingGamesList = upcomingGames;
-    });
+    setState(
+      () {
+        upcomingGamesList = upcomingGames;
+      },
+    );
+  }
+}
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final controller = TextEditingController();
+
+  String search = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text('Enter the text in the box below.'),
+        Row(
+          children: [
+            SizedBox(
+              width: 800,
+              child: TextField(
+                textAlign: TextAlign.center,
+                controller: controller,
+              ),
+            ),
+            ElevatedButton(onPressed: () {}, child: const Text('Search')),
+            Text(search)
+          ],
+        ),
+      ],
+    );
+  }
+
+  void _onButtonPressed() {
+    setState(
+      () {
+        search = controller.text;
+      },
+    );
+  }
+}
+
+class CowWidget extends StatelessWidget {
+  const CowWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.network(
+            'https://media.rawg.io/media/screenshots/c9f/c9f20e71776b841a8b7cf21917a7a15d.jpg'),
+      ],
+    );
   }
 }
