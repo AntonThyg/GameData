abb9ec4579f2c6f32f2b4841a57a29eb0501e823
Spencer Smith <spencersmith24@outlook.com>
2023-03-31 16:39:19 -0400

    make favorite page scrollable

--- lib/widgets/favorites_page_widget.dart
+++ lib/widgets/favorites_page_widget.dart
@@ -11,7 +11,7 @@ class FavoritesPage extends StatefulWidget {
 class _FavoritesPageState extends State<FavoritesPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: [
         for (Widget w in makeFavoritesWidgetList()) w,
       ],
