e4d8f22f745adeb46c2ebcec5aacb3804e5b727e
Antone Thygerson <arthygerson@bsu.edu>
2023-04-07 15:17:31 -0400

    Condensed favorites page to make use of new GameWidget class
    Signed-off-by: Antone Thygerson <arthygerson@bsu.edu>

--- lib/widgets/favorites_page_widget.dart
+++ lib/widgets/favorites_page_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:game_data/game.dart';
 import 'package:game_data/main.dart';
+
+import 'game_widget.dart';
 
 class FavoritesPage extends StatefulWidget {
   const FavoritesPage({super.key});
@@ -13,65 +16,8 @@ class _FavoritesPageState extends State<FavoritesPage> {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        for (Widget w in makeFavoritesWidgetList()) w,
+        for (Game g in favoritesList) GameWidget(g),
       ],
-    );
-  }
-
-  List<Widget> makeFavoritesWidgetList() {
-    return List.generate(
-      favoritesList.length,
-      (index) => Column(
-        children: [
-          Row(
-            children: [
-              SizedBox(
-                width: 200,
-                height: 200,
-                child: Image.network(favoritesList[index].imageUrl),
-              ),
-              const SizedBox(
-                width: 20,
-                height: 200,
-                child: null,
-              ),
-              Column(
-                children: [
-                  SizedBox(
-                    width: 600,
-                    height: 20,
-                    child: Text(favoritesList[index].title),
-                  ),
-                  const SizedBox(
-                    width: 200,
-                    height: 20,
-                  ),
-                  SizedBox(
-                    width: 600,
-                    height: 20,
-                    child: Text(favoritesList[index].releaseDateString),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        switch (favoritesList.contains(favoritesList[index])) {
-                          case true:
-                            favoritesList.remove(favoritesList[index]);
-                            break;
-                          case false:
-                            favoritesList.add(favoritesList[index]);
-                            break;
-                        }
-                      });
-                    },
-                    child: const Icon(Icons.thumb_up_sharp),
-                  )
-                ],
-              )
-            ],
-          ),
-        ],
-      ),
     );
   }
 }
