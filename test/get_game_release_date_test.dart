0944e1da0afb08474461ebbff895133a84479caa
Evan Greenfield <evangreenfield@live.com>
2023-04-19 16:35:18 -0400

    fix game release date tests

--- test/get_game_release_date_test.dart
+++ test/get_game_release_date_test.dart
@@ -2,26 +2,33 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:game_data/game.dart';
 import 'package:game_data/game_parser.dart';
 
 void main() {
-  GameParser gameCreator = GameParser();
+  GameParser gameParser = GameParser();
 
   final testFile = File('test/minecraft.json');
   final stringData = testFile.readAsStringSync();
   final decodedJsonObject = jsonDecode(stringData);
+  Game minecraft = Game(
+      decodedJsonObject["name"],
+      decodedJsonObject["description"],
+      decodedJsonObject["metacritic"],
+      DateTime.parse(decodedJsonObject["released"]),
+      decodedJsonObject["background_image"]);
 
   test("if the release date is 2009-05-10, getMonthName will return 'May.'",
       () {
-    String expected =
-        gameCreator.getMonthName(DateTime.parse(decodedJsonObject["released"]));
+    String expected = gameParser.getMonthName(minecraft.releaseDate);
     expect(expected, "May");
   });
 
   test(
-      "if the release date is 2009-05-10, getGameReleaseDate will return May 10, 2009",
+      "if the release date is 2009-05-10, getGameReleaseDateString will return May 10, 2009",
       () {
-    String expected = gameCreator.getGameReleaseDate(decodedJsonObject);
+    String expected =
+        gameParser.getGameReleaseDateString(minecraft.releaseDate);
     expect(expected, "May 10, 2009");
   });
 }
