77da374595a31f83aa4a3ab8f26070bfe4d79905
Evan Greenfield <evangreenfield@live.com>
2023-04-21 16:39:23 -0400

    make game_page_widget easier for users to navigate

--- lib/widgets/game_page_widget.dart
+++ lib/widgets/game_page_widget.dart
@@ -18,63 +18,95 @@ class _GamePageState extends State<GamePage> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(right: 8.0, top: 8.0, bottom: 8.0),
+      padding: const EdgeInsets.all(16.0),
       child: ListView(
         children: [
           ElevatedButton(
-              onPressed: () => {Navigator.pop(context)},
-              child: const Text("Back")),
-          Padding(
-            padding: const EdgeInsets.only(top: 8.0),
-            child: SizedBox(
+            onPressed: () => Navigator.pop(context),
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+            child: const Text("Back"),
+          ),
+          const SizedBox(height: 24),
+          SizedBox(
+            height: 400,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16),
               child: Image.network(
                 widget.game.imageUrl,
-                height: 400,
+                fit: BoxFit.cover,
               ),
             ),
           ),
+          const SizedBox(height: 24),
           SizedBox(
             child: Text(
               widget.game.title,
               textAlign: TextAlign.center,
-              style: const TextStyle(fontSize: 35),
+              style: const TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
             ),
           ),
-          const SizedBox(
-            height: 10,
-          ),
+          const SizedBox(height: 16),
           SizedBox(
             child: Text(
               GameParser().getGameReleaseDateString(widget.game.releaseDate),
               textAlign: TextAlign.center,
-              style: const TextStyle(fontSize: 20),
+              style: const TextStyle(fontSize: 24),
             ),
           ),
-          const SizedBox(
-            height: 10,
-          ),
+          const SizedBox(height: 16),
           SizedBox(
-            child: Text(
-              'Metacritic rating: ${widget.game.rating}/100',
-              textAlign: TextAlign.center,
-              style: const TextStyle(fontSize: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(Icons.star, size: 24, color: Colors.amber),
+                const SizedBox(width: 8),
+                Text(
+                  '${widget.game.rating}/100',
+                  style: const TextStyle(fontSize: 20),
+                ),
+              ],
             ),
           ),
-          const SizedBox(
-            height: 20,
-          ),
+          const SizedBox(height: 32),
           SizedBox(
-            width: 400,
+            width: double.infinity,
             child: Text(
               widget.game.description,
+              style: const TextStyle(fontSize: 18),
             ),
           ),
+          const SizedBox(height: 32),
           ElevatedButton(
             onPressed: () => setState(
-                () => widget.favoriteGames.setFavoriteState(widget.game)),
-            child: Icon(widget.favoriteGames.isFavorited(widget.game)
-                ? Icons.thumb_up
-                : Icons.thumb_up_alt_outlined),
+              () => widget.favoriteGames.setFavoriteState(widget.game),
+            ),
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(
+                  widget.favoriteGames.isFavorited(widget.game)
+                      ? Icons.thumb_up
+                      : Icons.thumb_up_alt_outlined,
+                  size: 24,
+                ),
+                const SizedBox(width: 8),
+                Text(
+                  widget.favoriteGames.isFavorited(widget.game)
+                      ? 'Liked'
+                      : 'Like',
+                  style: const TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
           ),
         ],
       ),
