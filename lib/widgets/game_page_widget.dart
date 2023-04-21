8cffcc53d295962021e98455a86ae81242cbe2dd
Spencer Smith <spencersmith24@outlook.com>
2023-04-21 16:52:51 -0400

    change background color of game page

--- lib/widgets/game_page_widget.dart
+++ lib/widgets/game_page_widget.dart
@@ -17,122 +17,125 @@ class GamePage extends StatefulWidget {
 class _GamePageState extends State<GamePage> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: ListView(
-        children: [
-          ElevatedButton(
-            onPressed: () => Navigator.pop(context),
-            style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
+    return Container(
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            ElevatedButton(
+              onPressed: () => Navigator.pop(context),
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              child: const Text("Back"),
+            ),
+            const SizedBox(height: 24),
+            SizedBox(
+              height: 400,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: Image.network(
+                  widget.game.imageUrl,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-            child: const Text("Back"),
-          ),
-          const SizedBox(height: 24),
-          SizedBox(
-            height: 400,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(16),
-              child: Image.network(
-                widget.game.imageUrl,
-                fit: BoxFit.cover,
+            const SizedBox(height: 24),
+            //title
+            SizedBox(
+              child: Text(
+                widget.game.title,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  decoration: TextDecoration.none,
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 24),
-          //title
-          SizedBox(
-            child: Text(
-              widget.game.title,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 48,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-                decoration: TextDecoration.none,
+            const SizedBox(height: 16),
+            //release date
+            SizedBox(
+              child: Text(
+                GameParser().getGameReleaseDateString(widget.game.releaseDate),
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 24,
+                  color: Colors.black,
+                  fontWeight: FontWeight.normal,
+                  decoration: TextDecoration.none,
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 16),
-          //release date
-          SizedBox(
-            child: Text(
-              GameParser().getGameReleaseDateString(widget.game.releaseDate),
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 24,
-                color: Colors.black,
-                fontWeight: FontWeight.normal,
-                decoration: TextDecoration.none,
-              ),
-            ),
-          ),
-          const SizedBox(height: 16),
-          //game rating
-          SizedBox(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(Icons.star, size: 24, color: Colors.amber),
-                const SizedBox(width: 8),
-                Text(
-                  '${widget.game.rating}/100',
-                  style: const TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
-                    fontWeight: FontWeight.normal,
-                    decoration: TextDecoration.none,
+            const SizedBox(height: 16),
+            //game rating
+            SizedBox(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(Icons.star, size: 24, color: Colors.amber),
+                  const SizedBox(width: 8),
+                  Text(
+                    '${widget.game.rating}/100',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.normal,
+                      decoration: TextDecoration.none,
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 32),
-          //game description
-          SizedBox(
-            width: double.infinity,
-            child: Text(
-              widget.game.description,
-              style: const TextStyle(
-                fontSize: 18,
-                color: Colors.black,
-                fontWeight: FontWeight.normal,
-                decoration: TextDecoration.none,
+                ],
               ),
             ),
-          ),
-          const SizedBox(height: 32),
-          ElevatedButton(
-            onPressed: () => setState(
-              () => widget.favoriteGames.setFavoriteState(widget.game),
-            ),
-            style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
+            const SizedBox(height: 32),
+            //game description
+            SizedBox(
+              width: double.infinity,
+              child: Text(
+                widget.game.description,
+                style: const TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                  fontWeight: FontWeight.normal,
+                  decoration: TextDecoration.none,
+                ),
               ),
             ),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(
-                  widget.favoriteGames.isFavorited(widget.game)
-                      ? Icons.thumb_up
-                      : Icons.thumb_up_alt_outlined,
-                  size: 24,
+            const SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: () => setState(
+                () => widget.favoriteGames.setFavoriteState(widget.game),
+              ),
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
                 ),
-                const SizedBox(width: 8),
-                Text(
-                  widget.favoriteGames.isFavorited(widget.game)
-                      ? 'Liked'
-                      : 'Like',
-                  style: const TextStyle(fontSize: 20),
-                ),
-              ],
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    widget.favoriteGames.isFavorited(widget.game)
+                        ? Icons.thumb_up
+                        : Icons.thumb_up_alt_outlined,
+                    size: 24,
+                  ),
+                  const SizedBox(width: 8),
+                  Text(
+                    widget.favoriteGames.isFavorited(widget.game)
+                        ? 'Liked'
+                        : 'Like',
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
